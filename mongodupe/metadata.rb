c58d7213b13465c5991ca2b692cbfd879a9e7cee
Jade Meskill <jade.meskill@gmail.com>
2014-08-14 16:42:21 -0700

    cron

---
+++ mongodupe/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodupe"
+description 'run cron on mongo'
+maintainer  "Jade Meskill"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'cron'
--- mongodupe/recipes/default.rb
+++ mongodupe/recipes/default.rb
@@ -1,6 +1,4 @@
-include_recipe "cron"
-
-cron_d 'copy-prod-to-staging' do
+cron 'copy-prod-to-staging' do
   command 'touch /tmp/copy-prod-to-staging'
   user    'root'
 end

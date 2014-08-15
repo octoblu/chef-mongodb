724d481e9a4504242b7ebbb8fbc7f618d35ee2e2
Jade Meskill <jade.meskill@gmail.com>
2014-08-14 17:04:52 -0700

    clone databases at midnight

--- mongodupe/recipes/default.rb
+++ mongodupe/recipes/default.rb
@@ -1,4 +1,6 @@
 cron 'copy-prod-to-staging' do
-  command 'touch /tmp/copy-prod-to-staging'
+  minute 5
+  hour 0
+  command 'mongo --host mongo-rs/localhost --eval "db=db.getSiblingDB(\'octoblu-staging\'); db.dropDatabase(); db=db.getSiblingDB(\'admin\'); db.runCommand({copydb:1, fromdb:\'octoblu\', todb:\'octoblu-staging\'}); db=db.getSiblingDB(\'meshblu-staging\'); db.dropDatabase(); db=db.getSiblingDB(\'admin\'); db.runCommand({copydb:1, fromdb:'meshblu', todb:'meshblu-staging'});"'
   user    'root'
 end

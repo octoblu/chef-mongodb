e502d14acee3dc9f53d6614af489b9933870c463
Jade Meskill <jade.meskill@gmail.com>
2014-08-17 12:01:05 -0700

    fix escaped quotes

--- mongodupe/recipes/default.rb
+++ mongodupe/recipes/default.rb
@@ -1,6 +1,6 @@
 cron 'copy-prod-to-staging' do
   minute 5
   hour 0
-  command 'mongo --host mongo-rs/localhost --eval "db=db.getSiblingDB(\'octoblu-staging\'); db.dropDatabase(); db=db.getSiblingDB(\'admin\'); db.runCommand({copydb:1, fromdb:\'octoblu\', todb:\'octoblu-staging\'}); db=db.getSiblingDB(\'meshblu-staging\'); db.dropDatabase(); db=db.getSiblingDB(\'admin\'); db.runCommand({copydb:1, fromdb:'meshblu', todb:'meshblu-staging'});"'
+  command 'mongo --host mongo-rs/localhost --eval "db=db.getSiblingDB(\'octoblu-staging\'); db.dropDatabase(); db=db.getSiblingDB(\'admin\'); db.runCommand({copydb:1, fromdb:\'octoblu\', todb:\'octoblu-staging\'}); db=db.getSiblingDB(\'meshblu-staging\'); db.dropDatabase(); db=db.getSiblingDB(\'admin\'); db.runCommand({copydb:1, fromdb:\'meshblu\', todb:\'meshblu-staging\'});"'
   user    'root'
 end
